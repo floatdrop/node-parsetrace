5942d5920529efff774060198929df608c85b1b7
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2014-01-02 18:56:44 +0600

    Implemented toString method

--- test/parsetrace.js
+++ test/parsetrace.js
@@ -8,45 +8,52 @@ var assert = require('power-assert');
 describe('parsetrace', function () {
     describe('simple.mock', function () {
         before(function () {
-            this.trace = parsetrace(error('simple.mock')).object();
+            this.trace = parsetrace(error('simple.mock'));
+            this.object = this.trace.object();
+            this.string = this.trace.toString();
+            this.stack = error('simple.mock').stack;
             this.answers = error('simple.mock').answers;
         });
 
         it('should parse error message', function () {
-            assert(this.trace.error === 'simple.js', 'Error message is corrupted');
+            assert.equal(this.object.error, 'simple.js', 'Error message is corrupted');
         });
 
         it('should parse stack frames', function () {
-            assert(this.trace.frames);
-            assert(this.trace.frames.length === 8);
+            assert.ok(this.object.frames);
+            assert.equal(this.object.frames.length, 8);
         });
 
         it('should parse frame function names', function () {
-            this.trace.frames.forEach(function (frame, i) {
+            this.object.frames.forEach(function (frame, i) {
                 var answer = this.answers.functions[i];
-                assert(frame.function === answer);
+                assert.equal(frame.function, answer);
             }.bind(this));
         });
 
         it('should parse frame lines', function () {
-            this.trace.frames.forEach(function (frame, i) {
+            this.object.frames.forEach(function (frame, i) {
                 var answer = this.answers.lines[i];
-                assert(frame.line === answer);
+                assert.equal(frame.line, answer);
             }.bind(this));
         });
 
         it('should parse frame columns', function () {
-            this.trace.frames.forEach(function (frame, i) {
+            this.object.frames.forEach(function (frame, i) {
                 var answer = this.answers.columns[i];
-                assert(frame.column === answer);
+                assert.equal(frame.column, answer);
             }.bind(this));
         });
 
         it('should parse frame file', function () {
-            this.trace.frames.forEach(function (frame, i) {
+            this.object.frames.forEach(function (frame, i) {
                 var answer = this.answers.files[i];
-                assert(frame.file === answer);
+                assert.equal(frame.file, answer);
             }.bind(this));
+        });
+
+        it('should compose stack string', function () {
+            assert.equal(this.string, this.stack);
         });
     });
 });
