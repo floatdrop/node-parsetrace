9135f5ef7ee30f78c83f9a096b36392f3b15786c
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2014-01-02 14:04:51 +0600

    Added tests

---
+++ test/main.js
@@ -0,0 +1,8 @@
+/* global describe, it, require, beforeEach */
+'use strict';
+
+describe('parsetrace', function () {
+    it('should requires without error', function () {
+        require('..');
+    });
+});
