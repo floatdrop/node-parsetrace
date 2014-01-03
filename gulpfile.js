b4db90d92fcd97822c84bc2d847b4dbe5fa9c24d
Vsevolod Strukchinsky <floatdrop@yandex-team.ru>
2014-01-03 20:53:33 +0600

    Got gulp to run mocha with watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,25 @@
 
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var batch = require('gulp-batch');
+
+gulp.task('mocha', function (cb) {
+    gulp.src(['test/*.js'])
+        .pipe(mocha({ reporter: 'list' }))
+        .on('error', function (err) {
+            console.log(err.stack);
+            cb();
+        })
+        .on('end', cb);
+});
 
 gulp.task('watch', function () {
-    gulp.watch(['test/**', 'lib/**'], function () {
-        gulp.src(['test/*.js'])
-            .pipe(mocha({ reporter: 'list' }))
-            .on('error', function () {
-                // Too Bad
-            });
-    });
+    gulp.watch(['test/**', 'lib/**'], batch(function (events, cb) {
+        gulp.run('mocha', cb);
+    }));
 });
 
 gulp.task('default', function () {
+    gulp.run('mocha');
     gulp.run('watch');
 });
